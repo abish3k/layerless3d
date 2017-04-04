757c82b1f48d9c9cffa2378ad465dced6b4aac9c
nicholas <nicholas.desouza@uoit.net>
2017-04-04 17:00:35 -0400

    added scaling to display

--- src/display.py
+++ src/display.py
@@ -17,8 +17,8 @@ class Display:
         size = [900, 600]
         info_object = pygame.display.Info()
         screen_size = (info_object.current_w, info_object.current_h)
-        # screen = pygame.display.set_mode(screen_size, pygame.FULLSCREEN)
-        screen = pygame.display.set_mode(size)
+        screen = pygame.display.set_mode(screen_size, pygame.FULLSCREEN)
+        # screen = pygame.display.set_mode(size)
 
         # Loop until the user clicks the close button.
         done = False
@@ -34,20 +34,18 @@ class Display:
         #     if event.type == pygame.QUIT:  # If user clicked close
         #         done = True  # Flag that we are done so we exit this loop
 
-        surfaces = []
         for layer in slices:
             surface = pygame.Surface((figure['max']['x'], figure['max']['y']))
             surface.fill(BLACK)
             for pair in layer:
                 pygame.draw.line(surface, WHITE, pair[0], pair[1], 5)
             scaled = pygame.transform.scale(surface, (int(figure['max']['x'] * 2), int(figure['max']['y'] * 2)))
-            screen.blit(scaled, (0, 0))
+            screen.blit(scaled, (int(screen_size[0]/6), int(screen_size[1]/8)))
             pygame.display.flip()
             clock.tick(30)
 
         print("Status: Finished Outputting Slices")
         pygame.quit()
-
 
 if __name__ == '__main__':
     import pickle
